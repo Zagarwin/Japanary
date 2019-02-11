36bcaf5f06428b0f8dd60249a073745099acdff9
bordy augustin <abordy2@pc277.deptinfo-st.univ-fcomte.fr>
2019-02-11 16:26:55 +0100

    some menu

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.get('/', function(req, res) {
 
 /*** Gestion des clients et des connexions ***/
 var clients = {};       // id -> socket
+var games = {};
 
 // Quand un client se connecte, on le note dans la console
 io.on('connection', function (socket) {
@@ -31,7 +32,7 @@ io.on('connection', function (socket) {
      *  Doit être la première action après la connexion.
      *  @param  id  string  l'identifiant saisi par le client
      */
-    socket.on("login", function(id) {
+    /*socket.on("login", function(id) {
         while (clients[id]) {
             id = id + "(1)";   
         }
@@ -45,65 +46,12 @@ io.on('connection', function (socket) {
         socket.broadcast.emit("message", { from: null, to: null, text: currentID + " a rejoint la discussion", date: Date.now() } );
         // envoi de la nouvelle liste à tous les clients connectés 
         io.sockets.emit("liste", Object.keys(clients));
+    });*/
+    
+    socket.on("new_game", function(new_game) {
+	
     });
-    
-    
-    /**
-     *  Réception d'un message et transmission à tous.
-     *  @param  msg     Object  le message à transférer à tous  
-     */
-    socket.on("message", function(msg) {
-        console.log("Reçu message");   
-        // si jamais la date n'existe pas, on la rajoute
-        msg.date = Date.now();
-        // si message privé, envoi seulement au destinataire
-        if (msg.to != null && clients[msg.to] !== undefined) {
-            console.log(" --> message privé");
-            clients[msg.to].emit("message", msg);
-            if (msg.from != msg.to) {
-                socket.emit("message", msg);
-            }
-        }
-        else {
-            console.log(" --> broadcast");
-            io.sockets.emit("message", msg);
-        }
-    });
-    
-
-    /** 
-     *  Gestion des déconnexions
-     */
-    
-    // fermeture
-    socket.on("logout", function() { 
-        // si client était identifié (devrait toujours être le cas)
-        if (currentID) {
-            console.log("Sortie de l'utilisateur " + currentID);
-            // envoi de l'information de déconnexion
-            socket.broadcast.emit("message", 
-                { from: null, to: null, text: currentID + " a quitté la discussion", date: Date.now() } );
-                // suppression de l'entrée
-            delete clients[currentID];
-            // envoi de la nouvelle liste pour mise à jour
-            socket.broadcast.emit("liste", Object.keys(clients));
-        }
-    });
-    
-    // déconnexion de la socket
-    socket.on("disconnect", function(reason) { 
-        // si client était identifié
-        if (currentID) {
-            // envoi de l'information de déconnexion
-            socket.broadcast.emit("message", 
-                { from: null, to: null, text: currentID + " vient de se déconnecter de l'application", date: Date.now() } );
-                // suppression de l'entrée
-            delete clients[currentID];
-            // envoi de la nouvelle liste pour mise à jour
-            socket.broadcast.emit("liste", Object.keys(clients));
-        }
-        console.log("Client déconnecté");
-    });
+   
     
     
 });
--- public/js/japanary.js
+++ public/js/japanary.js
@@ -5,8 +5,8 @@ var msg;
 var socket;
 var clients = [];
 
-document.getElementById("btnConnecter").addEventListener("click", function() {
-	id = document.getElementById("pseudo").value;
+document.getElementById("btnJoin").addEventListener("click", function() {
+	id = document.getElementById("nickname").value;
 	if (id == "") {
 		return;
 	}
@@ -19,11 +19,24 @@ document.getElementById("btnConnecter").addEventListener("click", function() {
 	document.getElementById("login").innerHTML = id;
 });
 
-function party_create_listener() {
-	var new_party = { alphabet : undefined, max_delay : 0,  laps_number : 0, is_private : false };
-	document.getElementById("btnCreate").addEventListener("click", function() {
-		document.getElementById("settings").style.display = "block";
+document.getElementById("btnCreate").addEventListener("click", function() {
+	id = document.getElementById("nickname").value;
+	if (id == "") {
+		return;
+	}
+	document.getElementById("settings").style.display = "block";
+	document.getElementById("login").style.display = "none";
+	create_game_listener();
+});
+
+function create_game_listener() {
+	document.getElementbyId("btnConfirmCreate").addEventListener("click", function() {
+		var new_game = { owner : id, alphabet : undefined, max_delay : 0,  laps_number : 0, is_private : false };
+		/*
+			Init fields
+		*/		
+		socket.emit("new_game", new_game);			
 	});
-}
+} 
 
 }
